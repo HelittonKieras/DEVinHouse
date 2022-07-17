2b7a47b25ad643d88310f3fed6759eff2cd5f6ae
Helitton Orlando Kieras <108702108+HelittonKieras@users.noreply.github.com>
2022-07-16 21:03:48 -0300

    Add files via upload
    Você foi contratado por um supermercado para catalogar a compra de uma cliente. Assim que a aplicação começa deverá ser perguntado em um prompt qual produto o cliente deseja comprar, só serão aceitas as seguintes respostas:
    
    (1) Hortifruti
    (2) Laticínios
    (3) Carnes
    (4) Peixes
    (5) Aves
    (6) Fechar pedido
    Após fazer a seleção do produto pergunte a quantidade de itens que o cliente irá comprar.
    Apenas se o cliente selecionar a opção 6 é que a pergunta deverá parar de ser repetida.
    Ao selecionar a opção 6 imprima o produto que o cliente pegou em maior quantidade.

---
+++ Semana 02/[M1S02] Ex 9/index.js
@@ -0,0 +1,63 @@
+var compra,
+  maiorQtd = 0,
+  itemMaiorQtd = 0,
+  open = true,
+  qtd = 0,
+  hort = 0,
+  lat = 0,
+  car = 0,
+  pei = 0,
+  ave = 0,
+  array = [],
+  itens = ["Hortifruti", "Laticícios", "Carnes", "Peixes", "Aves"];
+
+while (open == true) {
+  compra = parseFloat(
+    window.prompt(
+      "Digite o número referente à sua compra conforme abaixo: \n\n(1) Hortifruti \n(2) Laticínios \n(3) Carnes \n(4) Peixes \n(5) Aves \n(6) Fechar pedido"
+    )
+  );
+  
+  if (compra >= 1 && compra < 6) {
+    qtd = parseInt(window.prompt("Digite a quantidade"));
+  } else if (compra == 6) {
+  } else {
+    window.alert("Este valor não existe. Digite um dos valores da lista.");
+  }
+  switch (compra) {
+    case 1:
+      hort += qtd;
+      break;
+    case 2:
+      lat += qtd;
+      break;
+    case 3:
+      car += qtd;
+      break;
+    case 4:
+      pei += qtd;
+      break;
+    case 5:
+      ave += qtd;
+      break;
+    case 6:
+      array.push(hort, lat, car, pei, ave);
+      
+      for (i = 0; i < 5; i++) {
+        if (maiorQtd < array[i]) {
+          maiorQtd = array[i];
+          itemMaiorQtd = itens[i];
+        }
+      }
+
+      
+      if (maiorQtd == 0) {
+        window.alert(`Você não selecionou nenhum ítem.\n\nVolte sempre.`);
+      } else {
+        window.alert(
+          `Compra finalizada. O ítem de maior quantidade nesta compra foi o ${itemMaiorQtd} com ${maiorQtd} unidade(s).\n\nVolte sempre.`
+        );
+      }
+      open = false;
+  }
+}
